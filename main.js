24290817f082d49b8986748e90cb3595449b3996
yuliamrg <yuliam.rivera@outlook.com>
2022-08-29 00:53:43 -0500

    inicio del proyecto

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+
+function startGame() {
+    // game.fillRect(0,0,100,100);
+    /* game.fillRect(0,0,100,100);
+    game.clearRect(50,500,50,50); */
+    game.font = '25px Verdana';
+    game.fillStyle = 'purple';
+    game.textAlign = 'center';
+    game.fillText('Platzi', 25, 25);
+}
